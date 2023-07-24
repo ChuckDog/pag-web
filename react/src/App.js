343f805118c99ad9f9a2cb5e5cbb6ae04f163546
ChuckDog <xuzhong2012@my.fit.edu>
2023-07-24 09:50:22 +0800

    feat: react demo test

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { PAGInit } from 'libpag';
 
+let PAG,
+  pagView,
+  url = './like.pag';
+
 function App() {
+  const doChange = useRef(false);
+
+  const handleChange = () => (doChange.current = true);
+  const handleRepeat = () => {
+    if (doChange.current) {
+      if (url === './like.pag') {
+        url = './snowman.pag';
+      } else {
+        url = './like.pag';
+      }
+      fetch(url)
+        .then((response) => response.arrayBuffer())
+        .then(async (buffer) => {
+          const pagFile = await PAG.PAGFile.load(buffer);
+          pagView.setComposition(pagFile);
+          doChange.current = false;
+        });
+    }
+  };
+
   useEffect(() => {
-    PAGInit().then((PAG) => {
-      const url = './like.pag';
+    PAGInit().then((p) => {
+      PAG = p;
       fetch(url)
         .then((response) => response.arrayBuffer())
         .then(async (buffer) => {
@@ -13,17 +37,24 @@ function App() {
           const canvas = document.getElementById('pag');
           canvas.width = pagFile.width();
           canvas.height = pagFile.height();
-          const pagView = await PAG.PAGView.init(pagFile, canvas);
+          pagView = await PAG.PAGView.init(pagFile, canvas);
+          pagView.addListener('onAnimationRepeat', handleRepeat);
           pagView.setRepeatCount(0);
           await pagView.play();
         });
     });
-  });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <canvas id="pag"></canvas>
       </header>
+      <div>
+        <button onClick={handleChange} style={{ position: 'absolute', top: 20, left: 20 }}>
+          Change
+        </button>
+      </div>
     </div>
   );
 }
